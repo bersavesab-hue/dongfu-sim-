0aaf60405ff5ef702fb0f16cbbc7a38570dff02e
bersavesab-hue <bersavesab@gmail.com>
2026-09-23 21:14:43 +0800

    feat: register model atlas frames

---
+++ src/render/model-atlas.js
@@ -0,0 +1,16 @@
+export const MODEL_ATLAS_URL = new URL("../assets/model-atlas.svg", import.meta.url).href;
+export const MODEL_CELL_SIZE = 160;
+
+export const BUILDING_MODEL_FRAMES = Object.freeze({
+  residence: 0,
+  farm: 1,
+  workshop: 2,
+  canteen: 3,
+  warehouse: 4,
+});
+
+export const RESIDENT_MODEL_FRAMES = Object.freeze({
+  "resident-1": 5,
+  "resident-2": 6,
+  "resident-3": 7,
+});
