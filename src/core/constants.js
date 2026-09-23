2ab2b8d66109c6af56b659397d0e3bc96be2d8a6
bersavesab-hue <bersavesab@gmail.com>
2026-09-23 18:59:15 +0800

    feat(core): add map and resource constants

---
+++ src/core/constants.js
@@ -0,0 +1,13 @@
+export const MAP_WIDTH = 128;
+export const MAP_HEIGHT = 128;
+export const CHUNK_SIZE = 16;
+export const BUILDABLE_MIN_X = 54;
+export const BUILDABLE_MAX_X = 73;
+export const BUILDABLE_MIN_Y = 54;
+export const BUILDABLE_MAX_Y = 73;
+
+export const RESOURCE_TYPES = Object.freeze([
+  "food",
+  "materials",
+  "incense",
+]);
